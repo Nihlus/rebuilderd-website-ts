6ad37adde9bb9162066b7bf228a2bfd214a7b812
Jarl Gullberg <jarl.gullberg@gmail.com>
2025-05-11 12:41:57 +0200

    Implement auto-ranging and continuous data refresh.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {ThemeProvider, createTheme} from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import {PackageTable} from "./PackageTable.tsx";
 import {Box, Grid} from "@mui/material";
-import {useEffect, useMemo, useState} from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
 import RebuilderdAPI, {PackageRelease} from "./api/RebuilderdAPI.ts";
 import * as config from "./config/config.json";
 import {RebuildChart} from "./RebuildChart.tsx";
@@ -18,27 +18,47 @@ const theme = createTheme({
     }
 });
 
+function determineLocale(): string {
+    // All modern browsers support this. Should match what's used by localeCompare() etc.
+    const intl = window.Intl;
+    if (intl !== undefined) {
+        return intl.NumberFormat().resolvedOptions().locale;
+    }
+
+    // Fall back to ranked choice locales, which are configured in the browser but aren't necessarily
+    // what's used in functions like localeCompare().
+    const languages = navigator.languages as (string[] | undefined);
+    if (languages !== undefined && languages.length > 0) {
+        return languages[0];
+    }
+
+    // Old standard.
+    return navigator.language ?? "en-US";
+}
 function App() {
     const api = useMemo(() => {
         return new RebuilderdAPI(new URL(config.rebuilderd_host));
     }, []);
 
     const [packages, setPackages] = useState<PackageRelease[] | null>(null);
-
-    useEffect(() => {
+    const fetchPackagesContinuously = useCallback(function fetchPackages() {
         api.listPackages().then(p => {
             setPackages(p);
         }).catch(() => {
             // TODO: do something with the error
         });
+
+        setTimeout(fetchPackages, 60000);
     }, [api]);
 
+    useEffect(fetchPackagesContinuously, [fetchPackagesContinuously]);
+
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={determineLocale().toLowerCase()}>
             <ThemeProvider theme={theme}>
                 <CssBaseline/>
                 <Grid container columns={2} spacing={6} padding={3}>
-                    <Grid container size="grow" paddingBottom={6}>
+                    <Grid container size="grow" paddingBottom={6} direction="column" spacing={1}>
                         <RebuildChart api={api} packages={packages} />
                     </Grid>
                     <Grid container size="grow">
--- src/RebuildChart.tsx
+++ src/RebuildChart.tsx
@@ -1,9 +1,10 @@
 import RebuilderdAPI, {PackageRelease} from "./api/RebuilderdAPI.ts";
-import {LineChart} from "@mui/x-charts";
+import {type ChartsAxisData, LineChart} from "@mui/x-charts";
 import {useTheme} from "@mui/material/styles";
 import {useMemo, useState} from "react";
 import {Box, Grid, MenuItem, Select} from "@mui/material";
 import {DateTimePicker} from "@mui/x-date-pickers/DateTimePicker";
+import dayjs from "dayjs";
 
 interface RebuildChartProperties {
     api: RebuilderdAPI;
@@ -16,6 +17,7 @@ interface RebuildChartSeries {
     goodSeries: number[];
     badSeries: number[];
     unknownSeries: number[];
+    selectedGranularity: ChartGranularity;
 }
 
 type ChartGranularity = "Month" | "Day" | "Hour" | "Auto";
@@ -23,7 +25,7 @@ type ChartGranularity = "Month" | "Day" | "Hour" | "Auto";
 function createDataSeries(
     packages: PackageRelease[] | null,
     granularity: ChartGranularity = "Auto",
-    end: Date = new Date(),
+    end?: Date,
     start?: Date
 ): RebuildChartSeries {
     const xAxis: Date[] = [];
@@ -37,7 +39,8 @@ function createDataSeries(
             xAxis: xAxis,
             goodSeries: goodSeries,
             badSeries: badSeries,
-            unknownSeries: unknownSeries
+            unknownSeries: unknownSeries,
+            selectedGranularity: "Month"
         }
     }
 
@@ -47,11 +50,13 @@ function createDataSeries(
             xAxis: xAxis,
             goodSeries: goodSeries,
             badSeries: badSeries,
-            unknownSeries: unknownSeries
+            unknownSeries: unknownSeries,
+            selectedGranularity: "Month"
         }
     }
 
     // default to the earliest value
+    end ??= new Date();
     start ??= sortedBuiltPackages[0].built_at!;
 
     // try to figure out a good data scope
@@ -62,13 +67,13 @@ function createDataSeries(
         const roughDaysBetween = Math.floor(timeDifference / 86400000);
         const roughMonthsBetween = Math.floor(timeDifference / 2628000000);
 
-        if (roughHoursBetween <= 100) {
+        if (roughHoursBetween <= 200) {
             granularity = "Hour";
         }
-        else if (roughDaysBetween <= 100) {
+        else if (roughDaysBetween <= 200) {
             granularity = "Day";
         }
-        else if (roughMonthsBetween <= 100) {
+        else if (roughMonthsBetween <= 200) {
             granularity = "Month";
         }
         else {
@@ -77,7 +82,7 @@ function createDataSeries(
         }
     }
 
-    const sampleDate = new Date(start);
+    let sampleDate = new Date(start);
     while (sampleDate <= end) {
         const packagesAtSample = sortedBuiltPackages?.filter(p => p.built_at! <= sampleDate);
 
@@ -102,7 +107,11 @@ function createDataSeries(
         badSeries.push(badPackageCount);
         unknownSeries.push(packages.length - goodPackageCount - badPackageCount);
 
-        // increment by one day (JS handles rollover)
+        if (sampleDate == end) {
+            break;
+        }
+
+        // increment by one unit (JS handles rollover)
         switch (granularity) {
             case "Month": {
                 sampleDate.setUTCMonth(sampleDate.getUTCMonth() + 1);
@@ -120,13 +129,36 @@ function createDataSeries(
                 throw new RangeError("Invalid data granularity: " + granularity);
             }
         }
+
+        // always capture the last data point as now
+        if (sampleDate > end) {
+            sampleDate = end;
+        }
     }
 
     return {
         xAxis: xAxis,
         goodSeries: goodSeries,
         badSeries: badSeries,
-        unknownSeries: unknownSeries
+        unknownSeries: unknownSeries,
+        selectedGranularity: granularity
+    }
+}
+
+function formatDate(date: Date, granularity: ChartGranularity): string {
+    switch (granularity) {
+        case "Month": {
+            return `${date.getUTCFullYear()}-${date.getUTCMonth()}`
+        }
+        case "Day": {
+            return `${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDate()}`
+        }
+        case "Hour": {
+            return `${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDate()} ${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}`;
+        }
+        default: {
+            throw new RangeError("Invalid data granularity: " + granularity);
+        }
     }
 }
 
@@ -137,26 +169,30 @@ export function RebuildChart({packages, architecture}: RebuildChartProperties) {
 
     const [granularity, setGranularity] = useState<ChartGranularity>("Auto");
     const [start, setStart] = useState<Date | undefined>(undefined);
-    const [end, setEnd] = useState<Date>(new Date());
-    const seriesData = useMemo(() => createDataSeries(packages, granularity), [granularity, packages]);
+    const [end, setEnd] = useState<Date | undefined>(undefined);
+    const seriesData = useMemo(() => createDataSeries(packages, granularity, end, start), [end, granularity, packages, start]);
+    const [axisData, setAxisData] = useState<ChartsAxisData | null>();
     const theme = useTheme();
 
     return (
         <Grid container direction="column" size="grow">
-            <Grid container size="auto" direction="row" spacing={6} columns={3}>
-                <Select
-                    labelId="chart-granularity-select-label"
-                    id="chart-granularity-select"
-                    label="Granularity"
-                    value={granularity} onChange={g => setGranularity(g.target.value as ChartGranularity)}
-                >
-                    <MenuItem value="Auto">Auto</MenuItem>
-                    <MenuItem value="Month">Month</MenuItem>
-                    <MenuItem value="Day">Day</MenuItem>
-                    <MenuItem value="Hour">Hour</MenuItem>
-                </Select>
-                <DateTimePicker label="Start" />
-                <DateTimePicker label="End" />
+            <Grid container size="auto" direction="row" spacing={2} columns={3}>
+                <Box minWidth={200}>
+                    <Select
+                        labelId="chart-granularity-select-label"
+                        id="chart-granularity-select"
+                        label="Granularity"
+                        value={granularity} onChange={g => setGranularity(g.target.value as ChartGranularity)}
+                        fullWidth={true}
+                    >
+                        <MenuItem value="Auto">Auto</MenuItem>
+                        <MenuItem value="Month">Month</MenuItem>
+                        <MenuItem value="Day">Day</MenuItem>
+                        <MenuItem value="Hour">Hour</MenuItem>
+                    </Select>
+                </Box>
+                <DateTimePicker label="Start" value={dayjs(start ?? seriesData.xAxis[0])} onChange={v => setStart(v?.toDate())} ampm={false} />
+                <DateTimePicker label="End" value={dayjs(end)} onChange={v => setEnd(v?.toDate() ?? new Date())} ampm={false} />
             </Grid>
             <Grid size="grow">
                 <Box display="flex" alignItems="stretch" width="100%" height="100%">
@@ -167,7 +203,7 @@ export function RebuildChart({packages, architecture}: RebuildChartProperties) {
                             data: seriesData.xAxis,
                             min: seriesData.xAxis[0],
                             max: seriesData.xAxis[-1],
-                            valueFormatter: (date) => `${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDate()}`
+                            valueFormatter: (date) => formatDate(date, seriesData.selectedGranularity)
                         }]}
                         yAxis={[
                             {
@@ -175,11 +211,12 @@ export function RebuildChart({packages, architecture}: RebuildChartProperties) {
                             },
                         ]}
                         series={[
-                            { type: "line", data: seriesData.goodSeries, label: "GOOD", stack: "total", area: true, showMark: false, color: theme.palette.success.dark },
-                            { type: "line", data: seriesData.badSeries, label: "BAD", stack: "total", area: true, showMark: false, color: theme.palette.error.dark },
-                            { type: "line", data: seriesData.unknownSeries, label: "UNKWN", stack: "total", area: true, showMark: false, color: theme.palette.warning.dark }
+                            { id: "GOOD", type: "line", data: seriesData.goodSeries, label: "GOOD", stack: "total", area: true, showMark: false, color: theme.palette.success.dark },
+                            { id: "BAD", type: "line", data: seriesData.badSeries, label: "BAD", stack: "total", area: true, showMark: false, color: theme.palette.error.dark },
+                            { id: "UNKWN", type: "line", data: seriesData.unknownSeries, label: "UNKWN", stack: "total", area: true, showMark: false, color: theme.palette.warning.dark }
                         ]}
                         loading={packages === null}
+                        onAxisClick={(_, d) => setAxisData(d)}
                     />
                 </Box>
             </Grid>
